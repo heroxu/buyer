5f60e4a70b23c55fad1261ab188eade1dc956ab8
Tia <raot@stjf.com>
2018-04-03 09:37:10 +0800

    【我的】信息动态保存的sp里，修改页面显示；

--- buyer/app/src/main/java/com/smyy/sharetour/buyer/fragment/MyFragment.java
+++ buyer/app/src/main/java/com/smyy/sharetour/buyer/fragment/MyFragment.java
@@ -1,12 +1,21 @@
 package com.smyy.sharetour.buyer.fragment;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.smyy.sharetour.buyer.Constants;
 import com.smyy.sharetour.buyer.R;
 import com.smyy.sharetour.buyer.base.mvp.BaseMvpFragment;
 import com.smyy.sharetour.buyer.base.mvp.IBasePresenter;
+import com.smyy.sharetour.buyer.my.model.UserInfo;
+import com.smyy.sharetour.buyer.util.SharePreferenceUtil;
 import com.smyy.sharetour.buyer.view.RedImageView;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -16,11 +25,22 @@ import butterknife.OnClick;
  */
 
 public class MyFragment extends BaseMvpFragment {
-    @BindView(R.id.fake_status_bar)
-    View fake_status_bar;
+    @BindView(R.id.tv_my_username)
+    TextView tvUsername;
+    @BindView(R.id.tv_my_user_intro)
+    TextView tvUserIntro;
+    @BindView(R.id.iv_my_avatar)
+    ImageView ivAvatar;
+    @BindView(R.id.riv_my_awaiting_payment)
+    RedImageView rivAwaitingPayment;
+    @BindView(R.id.riv_my_awaiting_shipment)
+    RedImageView rivAwaitingShipment;
+    @BindView(R.id.riv_my_awaiting_confirmation)
+    RedImageView rivAwaitingConfirmation;
+    @BindView(R.id.riv_my_dispute)
+    RedImageView rivDispute;
+    private UserInfo mUserInfo;
 
-    @BindView(R.id.icon)
-    RedImageView icon;
 
     @Override
     protected IBasePresenter createPresenter() {
@@ -34,56 +54,74 @@ public class MyFragment extends BaseMvpFragment {
 
     @Override
     protected void initData(Bundle bundle) {
-        //处理状态栏
-        StatusBarAdapter.updateStatusHeight(getActivity(), fake_status_bar, null);
         changeTitleBarColor();
-        icon.setText("5");
-        icon.setRedPointVisible(View.VISIBLE);
+        initUserInfo();
+    }
+
+    private void initUserInfo() {
+        SharePreferenceUtil sp = new SharePreferenceUtil(getContext(), Constants.MY_SP);
+        mUserInfo = sp.getBeanValue(Constants.MY_SP_USER_INFO, UserInfo.class);
+        if (mUserInfo == null) {
+            mUserInfo = new UserInfo("悠闲的伪牧师", "一只大榴莲，两梳大香蕉。", "",
+                    0, 0, 0, 0);
+        }
+
+        tvUsername.setText(mUserInfo.getUsername());
+        tvUserIntro.setText(mUserInfo.getUserIntro());
+
+        String filePath = mUserInfo.getAvatar();
+        if (TextUtils.isEmpty(filePath)) {
+            Glide.with(this).load(R.mipmap.user_avatar).into(ivAvatar);
+        } else {
+            File file = new File(filePath);
+            Glide.with(getContext()).load(file).into(ivAvatar);
+        }
+        sp.writeBeanValue(Constants.MY_SP_USER_INFO, mUserInfo);
     }
 
     private void changeTitleBarColor() {
         StatusBarAdapter.changeStatusBarColor(getActivity(), getResources().getColor(R.color.transparent));
     }
 
-    @OnClick({ R.id.iv_setting, R.id.iv_msg, R.id.iv_edit_username, R.id.iv_user_avatar,
-            R.id.tv_view_all_orders, R.id.lay_awaiting_payment, R.id.lay_awaiting_shipment, R.id.lay_awaiting_confirmation, R.id.lay_dispute,
-            R.id.tv_my_cart ,R.id.tv_my_demands ,R.id.tv_my_reviews ,R.id.tv_my_favorites ,R.id.tv_my_interests ,R.id.tv_help_and_feedback})
+    @OnClick({R.id.iv_my_setting, R.id.iv_my_msg, R.id.iv_my_edit_username, R.id.iv_my_avatar,
+            R.id.tv_my_view_all_orders, R.id.lay_my_awaiting_payment, R.id.lay_my_awaiting_shipment, R.id.lay_my_awaiting_confirmation, R.id.lay_my_dispute,
+            R.id.tv_my_cart, R.id.tv_my_demands, R.id.tv_my_reviews, R.id.tv_my_favorites, R.id.tv_my_interests, R.id.tv_help_and_feedback})
     public void onClick(View view) {
         switch (view.getId()) {
 
-            case R.id.iv_setting:
+            case R.id.iv_my_setting:
 
                 break;
 
-            case R.id.iv_msg:
+            case R.id.iv_my_msg:
 
                 break;
 
-            case R.id.iv_edit_username:
+            case R.id.iv_my_edit_username:
 
                 break;
 
-            case R.id.iv_user_avatar:
+            case R.id.iv_my_avatar:
 
                 break;
 
-            case R.id.tv_view_all_orders:
+            case R.id.tv_my_view_all_orders:
 
                 break;
 
-            case R.id.lay_awaiting_payment:
+            case R.id.lay_my_awaiting_payment:
 
                 break;
 
-            case R.id.lay_awaiting_shipment:
+            case R.id.lay_my_awaiting_shipment:
 
                 break;
 
-            case R.id.lay_awaiting_confirmation:
+            case R.id.lay_my_awaiting_confirmation:
 
                 break;
 
-            case R.id.lay_dispute:
+            case R.id.lay_my_dispute:
 
                 break;
 
