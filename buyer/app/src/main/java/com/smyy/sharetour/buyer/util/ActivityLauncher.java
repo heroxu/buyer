4e5edb7481eb34003064ccb96ddaa2ddff866a1e
wuzhenfei <wuzf2012@sina.com>
2018-04-08 13:27:39 +0800

    跳转到登录引导

--- buyer/app/src/main/java/com/smyy/sharetour/buyer/util/ActivityLauncher.java
+++ buyer/app/src/main/java/com/smyy/sharetour/buyer/util/ActivityLauncher.java
@@ -3,6 +3,11 @@ package com.smyy.sharetour.buyer.util;
 import android.content.Context;
 import android.content.Intent;
 
+import com.smyy.sharetour.buyer.GuideLoginActivity;
+import com.smyy.sharetour.buyer.LoginActivity;
+import com.smyy.sharetour.buyer.PwdLoginActivity;
+import com.smyy.sharetour.buyer.RegisterActivity;
+import com.smyy.sharetour.buyer.SetPwdActivity;
 import com.smyy.sharetour.buyer.ui.Test1Activity;
 import com.smyy.sharetour.buyer.ui.test2.Test2Activity;
 
@@ -25,27 +30,38 @@ public class ActivityLauncher {
         Intent intent = new Intent(context, cls);
         context.startActivity(intent);
     }
-
+    /**
+     * 跳转到登录引导
+     */
     public static void viewGuideLoginActivity(Context context) {
         Intent intent = new Intent(context, GuideLoginActivity.class);
         context.startActivity(intent);
     }
-
+    /**
+     * 跳转到登录
+     */
     public static void viewLoginActivity(Context context) {
         Intent intent = new Intent(context, LoginActivity.class);
         context.startActivity(intent);
     }
 
+    /**
+     * 跳转到注册
+     */
     public static void viewRegisterActivity(Context context) {
         Intent intent = new Intent(context, RegisterActivity.class);
         context.startActivity(intent);
     }
-
+    /**
+     * 跳转到设置密码
+     */
     public static void viewSetPwdActivity(Context context) {
         Intent intent = new Intent(context, SetPwdActivity.class);
         context.startActivity(intent);
     }
-
+    /**
+     * 跳转到密码登录
+     */
     public static void viewPwdLoginActivity(Context context) {
         Intent intent = new Intent(context, PwdLoginActivity.class);
         context.startActivity(intent);
