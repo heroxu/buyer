a942faade615b04ab34e4008587e89a9c48b9a2f
Tia <raot@stjf.com>
2018-04-24 20:44:11 +0800

    增加背包客钱包

--- buyer/app/src/main/java/com/smyy/sharetour/buyer/BackPacker/my/MyWalletActivity.java
+++ buyer/app/src/main/java/com/smyy/sharetour/buyer/BackPacker/my/MyWalletActivity.java
@@ -1,6 +1,7 @@
 package com.smyy.sharetour.buyer.BackPacker.my;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
@@ -52,5 +53,10 @@ public class MyWalletActivity extends BaseMvpActivity {
             default:
                 break;
         }
+    }
+
+    @Override
+    protected void initStatusBar() {
+        setStatusBar(Color.BLACK);
     }
 }
